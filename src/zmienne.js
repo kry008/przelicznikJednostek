9c12914fb0b2f22b11945506a7679216086d7978
kry008 <pkrzysaaaa@gmail.com>
2022-04-12 21:08:43 +0200

    Naprawa przeliczników 2

--- src/zmienne.js
+++ src/zmienne.js
@@ -10,22 +10,22 @@ var prefixTime = [
         "name": "sekunda",
         "value": "1"
     },
-    {"name": "minuta", "value": "60"},
-    {"name": "godzina", "value": "3600"},
+    {"name": "minuta", "value": 1/60},
+    {"name": "godzina", "value": 1/3600},
 ]
 var prefixWeight = [
-    {"name": "kilogram", "value": "1"},
-    {"name": "gram", "value": "0.001"},
-    {"name": "miligram", "value": "0.000001"},
+    {"name": "kilogram", "value": 1},
+    {"name": "gram", "value": 1000},
+    {"name": "miligram", "value": 1000000},
 ]
 var prefixMemory = [
-    {"name": "bajt", "value": "1"},
-    {"name": "kilobajt", "value": "1000"},
-    {"name": "megabajt", "value": "1000000"},
+    {"name": "bajt", "value": 1},
+    {"name": "kilobajt", "value": 1/1000},
+    {"name": "megabajt", "value": 1/1000000},
 ]
 var prefixSpace = [
-    {"name": "m3", "value": "1"},
-    {"name": "cm3", "value": "0.00001"},
+    {"name": "m3", "value": 1},
+    {"name": "cm3", "value": 1000000},
 ]
 
 export { prefixLong, prefixTime, prefixWeight, prefixMemory, prefixSpace };
